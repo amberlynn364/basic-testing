3e2f9ba0ae4bc142677c2194ba4914c9901dd8c2
amberlynn <amberlynn364@gmail.com>
2024-02-09 20:54:59 +0300

    feat: implement mocking lib

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,56 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const data = {
+      userId: 1,
+      id: 1,
+      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipitsuscipit recusandae consequuntur expedita et cumreprehenderit molestiae ut ut quas totamnostrum rerum est autem sunt rem eveniet architecto"
+    }
+    mockedAxios.get.mockImplementationOnce(() => Promise.resolve(data));
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockData = {
+      userId: 1,
+      id: 1,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+    };
+
+    const relativePath = '/posts/1';
+
+    mockedAxios.get = jest.fn().mockResolvedValue({ data: mockData });
+
+    (axios.create as jest.Mock).mockReturnValue(mockedAxios);
+
+    const result = await throttledGetDataFromApi(relativePath);
+
+    expect(result).toEqual(mockData);
+    expect(mockedAxios.get).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockData = {
+      userId: 1,
+      id: 1,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+    };
+
+    const relativePath = '/posts/1';
+
+    mockedAxios.get.mockResolvedValue({ data: mockData });
+
+    const result = await throttledGetDataFromApi(relativePath);
+
+    expect(result).toEqual(mockData);
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.get).toHaveBeenCalledWith(relativePath);
   });
 });
